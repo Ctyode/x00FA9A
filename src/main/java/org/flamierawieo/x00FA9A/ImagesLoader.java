422365b699967356b06e0cb753ee93bca7abb8e7
ctyode <irribarrenya@gmail.com>
2015-11-13 23:41:30 +0300

    Ну тут короче курсор теперь отображается лол))

--- src/main/java/org/flamierawieo/x00FA9A/ImagesLoader.java
+++ src/main/java/org/flamierawieo/x00FA9A/ImagesLoader.java
@@ -16,62 +16,6 @@ public class ImagesLoader {
         cachedImages = new HashMap<>();
     }
 
-    public static void whatDoesNicktaelSaid() {
-        Logger.getLogger("Nickta").log(Level.WARNING, "THIS DANK CODE IS 2COOL4U"); // MAJESTIC GLORIOUS
-    }
-
-
-//    private static List<String> getPathsFor(String path) throws IOException {
-//        File file = new File(path);
-//        List<String> paths;
-//        if(file.isDirectory()) {
-//            File[] files = file.listFiles();
-//            if(files != null) {
-//                for (File listedFile : files) {
-//                    getPathsFor(listedFile);
-//                }
-//            }
-//        } else {
-//            return file.getCanonicalPath();
-//        }
-//        return null; // TODO
-//    }
-
-//    private static void loadImages(String path) {
-//        File file = new File(path);
-//        if(file.isDirectory()) {
-//            File[] listedFiles = file.listFiles();
-//            if(listedFiles != null) {
-//                for(File listedFile : listedFiles) {
-//                    loadImages(listedFile.getAbsolutePath());
-//                }
-//            }
-//        } else {
-//            // cachedImages.put(path, new Image());
-//            // TODO
-//        }
-//    }
-//
-//    public static void loadImages() {
-//        loadImages("resources"); // TODO: root resources path
-//    }
-//
-//    public static Image getImage(String path) {
-//        return cachedImages.get(path);
-//    }
-//
-//    @Deprecated
-//    public static Map<String, Image> getImages(Predicate<String> predicate) {
-//        Map<String, Image> images = new HashMap<String, Image>();
-//        cachedImages.keySet().stream().filter(predicate).forEach(path -> images.put(path, cachedImages.get(path)));
-//        return images;
-//    }
-//
-//    ^ ^ ^ ^ ^
-//    | | | | |
-//    cute code by Nickta :3
-
-
     public static Image getImage(String path) {
         if(!cachedImages.containsKey(path)) {
             try {
@@ -81,6 +25,10 @@ public class ImagesLoader {
             }
         }
         return cachedImages.get(path);
+    }
+
+    public static void whatDoesNicktaelSaid() {
+        Logger.getLogger("Nickta").log(Level.WARNING, "THIS DANK CODE IS 2COOL4U"); // MAJESTIC GLORIOUS
     }
 
 }
--- src/main/java/org/flamierawieo/x00FA9A/client/views/StartMenu.java
+++ src/main/java/org/flamierawieo/x00FA9A/client/views/StartMenu.java
@@ -15,16 +15,23 @@ public class StartMenu extends View {
 
     public StartMenu(GameContainer container) {
         super(container);
+        int width = container.getWidth();
+        int height = container.getHeight();
+
         Image singleplayerImg = Images.SINGLEPLAYER_BUTTON.getImage();
         Image multiplayerImg = Images.MULTIPLAYER_BUTTON.getImage();
         Image optionsImg = Images.OPTIONS_BUTTON.getImage();
         Image exitImg = Images.EXIT_BUTTON.getImage();
-        int width = container.getWidth();
-        int height = container.getHeight();
-        singleplayerButton = new Button(singleplayerImg, (width / 2) - ((singleplayerImg.getWidth() / 2f)), height * 0.1f, () -> System.out.println("singleplayer"));
-        multiplayerButton = new Button(multiplayerImg, (width / 2) - ((multiplayerImg.getWidth() / 2f)), height * 0.3f, () -> System.out.println("multiplayer"));
-        optionsButton = new Button(optionsImg, (width / 2) - ((optionsImg.getWidth() / 2f)), height * 0.5f, () -> System.out.println("options"));
-        exitButton = new Button(exitImg, (width / 2) - ((exitImg.getWidth() / 2f)), height * 0.7f, () -> System.out.println("exit"));
+
+        singleplayerButton = new Button(singleplayerImg, (width / 2) -
+                ((singleplayerImg.getWidth() / 2f)), height * 0.1f);
+        multiplayerButton = new Button(multiplayerImg, (width / 2) -
+                ((multiplayerImg.getWidth() / 2f)), height * 0.3f);
+        optionsButton = new Button(optionsImg, (width / 2) -
+                ((optionsImg.getWidth() / 2f)), height * 0.5f);
+        exitButton = new Button(exitImg, (width / 2) -
+                ((exitImg.getWidth() / 2f)), height * 0.7f);
+
         addWidget(singleplayerButton);
         addWidget(multiplayerButton);
         addWidget(optionsButton);
