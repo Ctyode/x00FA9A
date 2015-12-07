9d674037c60cbb65210c1742f1912a76a789b1e1
Ctyode <irribarrenya@gmail.com>
2015-12-06 21:39:19 -0800

    Merge branch 'master' of /Users/flamie/x00FA9A with conflicts.

--- src/main/java/org/flamierawieo/x00FA9A/client/ui/widgets/SongList.java
+++ src/main/java/org/flamierawieo/x00FA9A/client/ui/widgets/SongList.java
@@ -6,6 +6,7 @@ import org.flamierawieo.x00FA9A.client.ui.Widget;
 
 import java.awt.*;
 import java.util.Collection;
+import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
@@ -15,35 +16,35 @@ public class SongList extends Widget {
 
         public static final float ITEM_HEIGHT = 0.1f;
 
-        private String artist;
-        private String songName;
         private String author;
+        private String songName;
+        private String artist;
         private byte availableDifficulties;
-        private Text artistText;
-        private Text songNameText;
         private Text authorText;
+        private Text songNameText;
+        private Text artistText;
 
-        public ListItem(String artist, String songName, String author, byte availableDifficulties) {
-            this.artist = artist;
-            this.songName = songName;
+        public ListItem(String author, String songName, String artist, byte availableDifficulties) {
             this.author = author;
+            this.songName = songName;
+            this.artist = artist;
             this.availableDifficulties = availableDifficulties;
 
-            artistText = new Text(artist, Fonts.ROBOTO_LIGHT.getFont().deriveFont(Font.PLAIN, 30.0f));
+            authorText = new Text(author, Fonts.ROBOTO_LIGHT.getFont().deriveFont(Font.PLAIN, 40.0f));
             songNameText = new Text(songName, Fonts.ROBOTO_LIGHT.getFont().deriveFont(Font.PLAIN, 40.0f));
-            authorText = new Text(author, Fonts.ROBOTO_LIGHT.getFont().deriveFont(Font.PLAIN, 20.0f));
+            artistText = new Text(artist, Fonts.ROBOTO_LIGHT.getFont().deriveFont(Font.PLAIN, 40.0f));
         }
 
-        public String getArtist() {
-            return artist;
+        public String getAuthor() {
+            return author;
         }
 
         public String getSongName() {
             return songName;
         }
 
-        public String getAuthor() {
-            return author;
+        public String getArtist() {
+            return artist;
         }
 
         public byte getAvailableDifficulties() {
@@ -51,16 +52,14 @@ public class SongList extends Widget {
         }
 
         public void draw(float x, float y) {
+            authorText.draw(x, y);
+            songNameText.draw(x, y + 0.04f);
             artistText.draw(x, y);
-            songNameText.draw(x, y + 0.03f);
-            authorText.draw(x, y + 0.06f);
         }
-
     }
 
     private Collection<ListItem> songs;
     private Predicate<ListItem> filter;
-    private float scroll;
 
     public SongList(Collection<ListItem> songs, float x, float y, float width, float height, float originX, float originY) {
         super(x, y, width, height, originX, originY);
@@ -72,26 +71,16 @@ public class SongList extends Widget {
         this(songs, x, y, width, height, 0.0f, 0.0f);
     }
 
-    public void removeFilter() {
-        filter = (i) -> true;
-    }
-
     public void setFilter(Predicate<ListItem> filter) {
         this.filter = filter;
     }
 
     @Override
-    public void onScroll(double x, double y) {
-        scroll -= y * 0.1;
-    }
-
-    @Override
     public void draw() {
-        float offset = scroll;
+        float offset = 0.0f;
         for(ListItem song : songs.stream().filter(filter).collect(Collectors.toList())) {
             song.draw(getAbsolutePositionX(), getAbsolutePositionY() + offset);
             offset += ListItem.ITEM_HEIGHT;
         }
     }
-
 }
