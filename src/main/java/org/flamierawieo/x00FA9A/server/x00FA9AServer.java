f0eeef221d9ca7cf55520859a900d78c4afcc9e9
Nickta Rawieo <rawieo@yandex.ru>
2015-12-10 00:36:53 +0400

    че

--- src/main/java/org/flamierawieo/x00FA9A/server/x00FA9AServer.java
+++ src/main/java/org/flamierawieo/x00FA9A/server/x00FA9AServer.java
@@ -1,4 +1,21 @@
 package org.flamierawieo.x00FA9A.server;
 
-public class x00FA9AServer {
+import org.flamierawieo.x00FA9A.server.net.AbstractConnection;
+import org.flamierawieo.x00FA9A.server.net.Server;
+
+import java.io.IOException;
+
+public class X00FA9AServer extends Thread implements Runnable {
+
+    Server server;
+
+    public X00FA9AServer() throws IOException {
+        server = new Server(AbstractConnection.class);
+    }
+
+    @Override
+    public void run() {
+
+    }
+
 }
