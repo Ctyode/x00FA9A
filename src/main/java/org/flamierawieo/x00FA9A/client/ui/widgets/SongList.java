205e340bf506b2c809cba8ae93999c08276fa59b
Ctyode <irribarrenya@gmail.com>
2015-12-09 20:34:25 -0800

    жсон )(

--- src/main/java/org/flamierawieo/x00FA9A/client/ui/widgets/SongList.java
+++ src/main/java/org/flamierawieo/x00FA9A/client/ui/widgets/SongList.java
@@ -1,86 +1,42 @@
 package org.flamierawieo.x00FA9A.client.ui.widgets;
 
-import org.flamierawieo.x00FA9A.client.Fonts;
-import org.flamierawieo.x00FA9A.client.graphics.Text;
 import org.flamierawieo.x00FA9A.client.ui.Widget;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
-import java.awt.*;
-import java.util.Collection;
-import java.util.List;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class SongList extends Widget {
 
-    public static class ListItem {
-
-        public static final float ITEM_HEIGHT = 0.1f;
-
-        private String author;
-        private String songName;
-        private String artist;
-        private byte availableDifficulties;
-        private Text authorText;
-        private Text songNameText;
-        private Text artistText;
-
-        public ListItem(String author, String songName, String artist, byte availableDifficulties) {
-            this.author = author;
-            this.songName = songName;
-            this.artist = artist;
-            this.availableDifficulties = availableDifficulties;
-
-            authorText = new Text(author, Fonts.ROBOTO_LIGHT.getFont().deriveFont(Font.PLAIN, 40.0f));
-            songNameText = new Text(songName, Fonts.ROBOTO_LIGHT.getFont().deriveFont(Font.PLAIN, 40.0f));
-            artistText = new Text(artist, Fonts.ROBOTO_LIGHT.getFont().deriveFont(Font.PLAIN, 40.0f));
-        }
-
-        public String getAuthor() {
-            return author;
-        }
-
-        public String getSongName() {
-            return songName;
-        }
-
-        public String getArtist() {
-            return artist;
-        }
-
-        public byte getAvailableDifficulties() {
-            return availableDifficulties;
-        }
-
-        public void draw(float x, float y) {
-            authorText.draw(x, y);
-            songNameText.draw(x, y + 0.04f);
-            artistText.draw(x, y);
-        }
-    }
-
-    private Collection<ListItem> songs;
-    private Predicate<ListItem> filter;
-
-    public SongList(Collection<ListItem> songs, float x, float y, float width, float height, float originX, float originY) {
+    public SongList(float x, float y, float width, float height, float originX, float originY) {
         super(x, y, width, height, originX, originY);
-        this.songs = songs;
-        this.filter = (i) -> true;
+
+        JSONParser parser = new JSONParser();
+        try {
+            JSONObject root = (JSONObject) parser.parse(new FileReader("res/json/song_cache.json"));
+            String uuid = (String) root.get("cache_uuid");
+            System.out.println(uuid);
+            JSONArray songs = (JSONArray) root.get("songs");
+            songs.forEach(s -> {
+                JSONObject song = (JSONObject) s;
+                System.out.println(song.get("artist") + " " + song.get("title") + " " + song.get("author"));
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
     }
 
-    public SongList(Collection<ListItem> songs, float x, float y, float width, float height) {
-        this(songs, x, y, width, height, 0.0f, 0.0f);
-    }
-
-    public void setFilter(Predicate<ListItem> filter) {
-        this.filter = filter;
+    public SongList(float x, float y, float width, float height) {
+        this(x, y, width, height, 0.0f, 0.0f);
     }
 
     @Override
     public void draw() {
-        float offset = 0.0f;
-        for(ListItem song : songs.stream().filter(filter).collect(Collectors.toList())) {
-            song.draw(getAbsolutePositionX(), getAbsolutePositionY() + offset);
-            offset += ListItem.ITEM_HEIGHT;
         }
-    }
+
 }
--- src/main/java/org/flamierawieo/x00FA9A/client/views/SongMenu.java
+++ src/main/java/org/flamierawieo/x00FA9A/client/views/SongMenu.java
@@ -8,8 +8,6 @@ import org.flamierawieo.x00FA9A.client.ui.ViewManager;
 import org.flamierawieo.x00FA9A.client.ui.widgets.SongList;
 import org.flamierawieo.x00FA9A.client.views.gamemods.SquareMode;
 
-import java.util.ArrayList;
-
 public class SongMenu extends View {
 
     private SongList songList;
@@ -32,12 +30,7 @@ public class SongMenu extends View {
                 .setPosition(0.8f, 0.5f)
                 .setSize(0.8f, 0.18f)
                 .setOrigin(0.5f, 0.5f).build();
-        songList = new SongList(new ArrayList<SongList.ListItem>() {{
-            add(new SongList.ListItem("Nicktael Rawieo", "Meow song", "wow", (byte) 0b00000000));
-            add(new SongList.ListItem("Renard", "The club", "wow", (byte) 0b00000000));
-            add(new SongList.ListItem("The Queenstons", "Figurehead", "wow", (byte) 0b00000000));
-            add(new SongList.ListItem("Renard", "TU4AR", "wow", (byte) 0b00000000));
-        }}, 0.5f, 0.5f, 0.5f, 0.5f);
+        songList = new SongList(0.5f, 0.5f, 0.5f, 0.5f);
 
         addWidget(selectModeBackground);
         addWidget(songListBackground);
